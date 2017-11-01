e7159e242fd8c721383ff1a4bf4c098f83dd937b
mpieszczyk <pieszczykm@gmail.com>
2017-11-01 15:23:58 +0100

    save data from form to object

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,41 @@
 (function(){
-  
+
+  $("#btn").click(function(e){
+
+    $(":input select, textarea").each(function() {
+
+       if($(this).val() === "") {
+
+         alert("Pola nie mogą być puste");
+
+       } else {
+
+         var jsonData = {};
+         var formData = $("#formCaseAdd").serializeArray();
+
+         $.each(formData, function() {
+
+           if (jsonData[this.name]) {
+             if (!jsonData[this.name].push) {
+               jsonData[this.name] = [jsonData[this.name]];
+             }
+             jsonData[this.name].push(this.value || '');
+           } else {
+             jsonData[this.name] = this.value || '';
+           }
+
+         });
+
+         $("#formCaseAdd").find("input, select, textarea").val("");
+
+         console.log(jsonData);
+
+         e.preventDefault();
+
+       }
+
+    });
+
+  });
+
 })();
