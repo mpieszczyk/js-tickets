7c2759495e77d19574959437c41a62b24e1318df
mpieszczyk <pieszczykm@gmail.com>
2017-11-06 23:21:59 +0100

    post&get data from db

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 (function(){
 
-  $("#btn").click(function(e){
-
+  function postData() {
+    // select form inputs
     $(":input select, textarea").each(function() {
-
+      // inputs simple validation
        if($(this).val() === "") {
 
          alert("Pola nie mogą być puste");
 
        } else {
-
+         // get data from inputs
          var jsonData = {};
          var formData = $("#formCaseAdd").serializeArray();
 
@@ -25,16 +25,40 @@
            }
 
          });
-
-         $("#formCaseAdd").find("input, select, textarea").val("");
-
-         console.log(jsonData);
-
-         e.preventDefault();
+         // send data to mongoDB
+         $.ajax({
+           url: "https://api.mongolab.com/api/1/databases/case-add-form/collections/case-add-form-items?apiKey=VLJbjCcSjJW8puhnqe4oFQfJmXAw30Qy",
+           type: "POST",
+           data: JSON.stringify(jsonData),
+           contentType: "application/json;charset=utf-8"
+         }).done(function(msg) {
+           alert("Dodano!");
+         });
 
        }
 
     });
+  }
+
+  function getData() {
+    $.ajax({
+      url: "https://api.mongolab.com/api/1/databases/case-add-form/collections/case-add-form-items?apiKey=VLJbjCcSjJW8puhnqe4oFQfJmXAw30Qy"
+    }).done(function(data) {
+      console.log(data);
+    });
+  }
+
+  // get button
+  $("#btn").click(function(e){
+    // form reload lock
+    e.preventDefault();
+
+    postData();
+
+    // reset form inputs
+    $("#formCaseAdd").find("input, select, textarea").val("");
+
+    getData();
 
   });
 
