582b914daf921e424d0acac6d1c4dbb5ee027186
mpieszczyk <pieszczykm@gmail.com>
2017-11-15 00:31:19 +0100

    add half working pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,25 +4,25 @@
     // select form inputs
     $(":input select, textarea").each(function() {
       // inputs simple validation
-       if($(this).val() === "") {
+       if( $(this).val() === "" ) {
 
-         alert("Pola nie mogą być puste");
+         alert("Pola nie mogą być puste")
 
        } else {
          // create empty object
-         var jsonData = {};
+         var jsonData = {}
          // get data from inputs
-         var formData = $("#formCaseAdd").serializeArray();
+         var formData = $("#formCaseAdd").serializeArray()
          // add data to object
          $.each(formData, function() {
 
-           if (jsonData[this.name]) {
-             if (!jsonData[this.name].push) {
-               jsonData[this.name] = [jsonData[this.name]];
+           if ( jsonData[this.name] ) {
+             if ( !jsonData[this.name].push ) {
+               jsonData[this.name] = [jsonData[this.name]]
              }
-             jsonData[this.name].push(this.value || '');
+             jsonData[this.name].push(this.value || '')
            } else {
-             jsonData[this.name] = this.value || '';
+             jsonData[this.name] = this.value || ''
            }
 
          });
@@ -33,7 +33,7 @@
            data: JSON.stringify(jsonData),
            contentType: "application/json;charset=utf-8"
          }).done(function(msg) {
-           alert("Dodano!");
+           alert("Dodano!")
          });
 
        }
@@ -45,38 +45,82 @@
     $.ajax({
       url: "https://api.mongolab.com/api/1/databases/case-add-form/collections/case-add-form-items?apiKey=VLJbjCcSjJW8puhnqe4oFQfJmXAw30Qy"
     }).done(function(tableData) {
-        // console.log(tableData);
-      var output;
-      $.each(tableData, function(key, tableData){
+      var output
+      $.each(tableData, function(key, tableData) {
         output += "<tr class='table-active'>";
-        output += "<td style='display: table-cell;'>" + tableData.caseNumber + "</td>";
-        output += "<td style='display: table-cell;'>" + tableData.firstName + "</td>";
-        output += "<td style='display: table-cell;'>" + tableData.lastName + "</td>";
-        output += "<td style='display: table-cell;'>" + tableData.date + "</td>";
-        output += "<td style='display: table-cell;'>" + tableData.place + "</td>";
-        output += "<td style='display: table-cell;'>" + tableData.person + "</td>";
-        output += "<td style='display: table-cell;'>" + tableData.describe + "</td>";
-        output += "</tr>";
-      });
+        output += "<td style='display: table-cell;'>" + tableData.caseNumber + "</td>"
+        output += "<td style='display: table-cell;'>" + tableData.firstName + "</td>"
+        output += "<td style='display: table-cell;'>" + tableData.lastName + "</td>"
+        output += "<td style='display: table-cell;'>" + tableData.date + "</td>"
+        output += "<td style='display: table-cell;'>" + tableData.place + "</td>"
+        output += "<td style='display: table-cell;'>" + tableData.person + "</td>"
+        output += "<td style='display: table-cell;'>" + tableData.describe + "</td>"
+        output += "<td style='display: table-cell;'><i class='fa fa-pencil-square fa-lg' aria-hidden='true'></i>&nbsp;<i class='fa fa-trash fa-lg' aria-hidden='true'></i></td>"
+        output += "</tr>"
+      })
 
-      $("#tableBodyData").html(output);
-      
-    });
+      $("#tableBodyData").html(output)
+      tabPagin()
+    })
   }
 
-  $(document).ready(function(){
+  // table pagination
+  function tabPagin() {
+    var table = "#myTab",
+    trnum = 0,
+    maxRows = parseInt($("#numOfRows").val()),
+    totalRows = $(table+" tbody tr").length
+    $(table + "tr:gt(0)").each(function() {
+      trnum++
+      if ( trnum > maxRows ) {
+        $(this).hide()
+      } else if ( trnum <= maxRows ) {
+        $(this).show()
+      }
+    })
+    if( totalRows > maxRows ) {
+      var pagenum = Math.ceil(totalRows/maxRows)
+      for(var i=1;i<=pagenum;) {
+        $(".pagination").append("<li class='page-item' data-page=" + i + ">\<a style='user-select: none;'' class='page-link'>" + i++ + "</a>\</li>").show()
+      }
+    } else {
+      $(".pagination").html("")
+      $(table + " tr:gt(0)").show
+    }
+    $(".pagination li:first-child").addClass("active")
+    $(".pagination li").on("click",function(){
+      var pagenum = $(this).attr("data-page"),
+      trIndex = 0
+      $(".pagination li").removeClass("active")
+      $(this).addClass("active")
+      $(table + " tr:gt(0)").each(function(){
+        trIndex++
+        if( trIndex > (maxRows*pagenum) || trIndex <= ((maxRows*pagenum)-maxRows) ) {
+          $(this).hide()
+        } else {
+          $(this).show()
+        }
+      })
+    })
+  }
+
+  $(document).ready(function() {
 
     getData();
 
     // get button
     $("#btn").click(function(e){
       // form reload lock
-      e.preventDefault();
-      postData();
-      getData();
+      e.preventDefault(e)
+      postData()
+      getData()
       // reset form inputs
       $("#formCaseAdd").find("input, select, textarea").val("");
     });
+
+    $("#numOfRows").on("change", function() {
+      tabPagin()
+    })
 
   });
 
--- assets/js/view.js
+++ assets/js/view.js
@@ -1,27 +1,22 @@
-(function(){
+(function() {
 
   function hideForm() {
 
-    $("#formHide").click(function(e){
-      e.preventDefault();
+    $("#formHide").click(function(e) {
+      e.preventDefault()
       if($("#buttonLabel").html() == "ZWIŃ FORMULARZ") {
-        $("#buttonLabel").html("ROZWIŃ FORMULARZ");
-        $(".fa").removeClass("fa-angle-double-up");
-        $(".fa").addClass("fa-angle-double-down");
+        $("#buttonLabel").html("ROZWIŃ FORMULARZ")
+        $(".fa").removeClass("fa-angle-double-up")
+        $(".fa").addClass("fa-angle-double-down")
       } else {
-        $("#buttonLabel").html("ZWIŃ FORMULARZ");
-        $(".fa").removeClass("fa-angle-double-down");
-        $(".fa").addClass("fa-angle-double-up");
-      };
-      $("#formCaseAdd").find(".row").toggleClass("d-none");
+        $("#buttonLabel").html("ZWIŃ FORMULARZ")
+        $(".fa").removeClass("fa-angle-double-down")
+        $(".fa").addClass("fa-angle-double-up")
+      }
+      $("#formCaseAdd").find(".row").toggleClass("d-none")
     })
-  };
-
-  function tablePagination() {
-    
   }
 
-  hideForm();
-  tablePagination()
+  hideForm()
 
 })();
