d4ce3324d4385025ad6bc69ecc3b84dde18cac17
Mateusz Pieszczyk <pieszczykm@gmail.com>
2022-05-25 18:33:13 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,35 @@
 (function() {
-
+    
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "case-form",
+    "database": "case-form",
+    "dataSource": "js-ticket",
+    "projection": {
+        "_id": 1
+    }
+});
+            
+var config = {
+    method: 'post',
+    url: 'https://data.mongodb-api.com/app/data-nvmza/endpoint/data/beta/action/findOne',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': '<API_KEY>'
+    },
+    data : data
+};
+            
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    
+    
 //post data to db
     function addData() {
 
